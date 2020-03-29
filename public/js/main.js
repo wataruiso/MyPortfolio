8d939fccdde481ebea27513b55e737f9747db601
Wataru Iso <shooky0510@gmail.com>
2020-03-29 21:17:11 +0900

    詳細サイトへのリンクを追加

--- public/js/main.js
+++ public/js/main.js
@@ -4,26 +4,67 @@ const hamburger_bar = $('.hamburger span');
 const hamburger = $('.hamburger');
 const nav = $('nav');
 const fadein = $('.fadein');
-const title = $('.section_title');
 const nav_li = $('nav li');
-const contact_link = $('.contact_link');
-const scroll = $('.scroll');
-let work_per_page = 1;
+const scroll_arrow = $('.scroll');
+const link_to_service_site = $('.link_to_service_site');
+const speed = 500;
 let back = $('.back');
 let next = $("#next");
 let prev = $("#prev");
 let carousel = $("#worklist");
 let flag = true;
+
+let work_per_page = 1;
 let works_length = $('#worklist li:last').index() + 1;
+let page = Math.ceil(works_length / work_per_page);
+let surplus = works_length % work_per_page;
+
 const intro_els = [
     $('#intro_wrapper_black'),
     $('.intro_wrapper'),
     $('.intro_wrapper'),
     hamburger_bar
 ];
+const anime_els_via_page = [
+    {
+        el: hamburger_bar,
+        class: `intro_animation${intro_els.length}`
+    },
+    {
+        el: fadein,
+        class: 'in'
+    },
+    {
+        el: $('.section_title'),
+        class: 'titlein'
+    },
+    {
+        el: nav_li,
+        class: 'nav_li_in_pc'
+    },
+    {
+        el: scroll_arrow,
+        class: 'flash'
+    },
+    {
+        el: $('.contact_link'),
+        class: 'slidein'
+    },
+];
 
-let page = Math.ceil(works_length / work_per_page);
-let surplus = works_length % work_per_page;
+function animationViaPage(isAdd) {
+    if(isAdd) {
+        anime_els_via_page.forEach(anime_el => {
+            anime_el.el.addClass(anime_el.class);
+        })
+    }else {
+        anime_els_via_page.forEach(anime_el => {
+            anime_el.el.removeClass(anime_el.class);
+        })
+    }
+}
+
+
 
 function getCurrent () {
     if(work_per_page === 1) $('#current').text($("#worklist li").eq(0).data('index'));
@@ -55,35 +96,27 @@ $(document).ready(function() {
         easing: 'easeOutCubic',
         onLeave: function() {
             if(nav.hasClass('show')) back.click();
-            fadein.css('opacity', '0');
-            title.removeClass('titlein');
-            hamburger_bar.removeClass(`intro_animation${intro_els.length}`);
-            contact_link.removeClass('slidein');
-            scroll.removeClass('flash');
-            nav_li.removeClass('nav_li_in_pc');
+            link_to_service_site.removeClass('show');
+            animationViaPage(0);
         },
-        afterLoad: function() {
-            fadein.css('opacity', '1');
-            title.addClass('titlein');
-            hamburger_bar.addClass(`intro_animation${intro_els.length}`);
-            contact_link.addClass('slidein');
-            scroll.addClass('flash');
-            nav_li.addClass('nav_li_in_pc');
+        afterLoad: function(anchorLink) {
+            if(anchorLink !== 'firstPage') link_to_service_site.addClass('show');
+            animationViaPage(1);
         },
         afterRender: function() {
             setTimeout(() => {
-                fadein.css('opacity', '1');
+                fadein.addClass('in');
                 nav_li.addClass('nav_li_in_pc');
-                scroll.addClass('flash');
+                scroll_arrow.addClass('flash');
             }, 7000)
         }
     });
 
-    for(let i = 1; i < intro_els.length + 1; i++) {
+    intro_els.forEach((intro_el, index)=> {
         setTimeout(() => {
-            intro_els[i - 1].addClass(`intro_animation${i}`);
-        }, 4000 + 1000 * i);
-    }
+            intro_el.addClass(`intro_animation${index + 1}`);
+        }, 4000 + 1000 * (index + 1));
+    }) 
 });
 
 hamburger.click(function() {
@@ -92,8 +125,8 @@ hamburger.click(function() {
         nav.addClass('show');
         setTimeout(() => {
             nav_li.addClass('show'); 
-        }, 500)
-    }, 500);
+        }, speed)
+    }, speed);
 })
 back.click(function() {
     nav_li.removeClass('show');
@@ -101,15 +134,15 @@ back.click(function() {
         nav.removeClass('show');
         setTimeout(() => {
             hamburger_bar.addClass(`intro_animation${intro_els.length}`);
-        }, 500)
-    }, 500);
+        }, speed)
+    }, speed);
 })
 next.click(function(){
     if(flag){
         flag = false;        
         carousel.animate({
         left: '-100%'
-        }, 500, 'swing', function(){
+        }, speed, 'swing', function(){
         for(let i = 0; i < work_per_page; i++){
         carousel.append($("#worklist li").eq(0));
         }
@@ -131,7 +164,7 @@ if(flag){
     left: '-100%'
     }).animate({
     left: 0
-    }, 500, 'swing', function(){    
+    }, speed, 'swing', function(){    
     flag = true;
     getCurrent();
     });
